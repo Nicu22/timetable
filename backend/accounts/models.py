d7340a21c40500e2bba115d5744de26bbd094448
Nicu22 <94984775+Nicu22@users.noreply.github.com>
2022-11-07 17:15:06 +0200

    Add files via upload

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser
 
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(unique=True)
+    username = models.CharField(unique=True, max_length=20, default="John")
     join_date = models.DateField(auto_now=True)
     is_admin = models.BooleanField(default=False)
 
